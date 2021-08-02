87bef2c503b45e8d61d3aee8ecee874dd75be49b
cuongvu3009 <39565575+cuongvu3009@users.noreply.github.com>
2021-08-02 13:36:13 +0300

    Update context-example.js

--- context-example.js
+++ context-example.js
@@ -3,7 +3,7 @@ import React, { useState, useContext, createContext } from "react";
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider>{children}</AppContext.Provider>;
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 };
 
 const useGlobalContext = () => {
