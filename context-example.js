7f18a7627a492a2643693a933df2868843def5c0
cuongvu3009 <39565575+cuongvu3009@users.noreply.github.com>
2021-08-02 13:35:41 +0300

    update

--- context-example.js
+++ context-example.js
@@ -1,24 +1,13 @@
-// Adding state to the context provider
+import React, { useState, useContext, createContext } from "react";
 
-// Import createContext() method and useState hook from React:
-import { createContext, useState } from "react";
+const AppContext = createContext();
 
-// Create new context:
-export const newContext = createContext();
-
-// Create new provider component:
-export const NewProvider = (props) => {
-  // Create local state:
-  const [state, setState] = useState("");
-
-  // Prepare values to share:
-  const val = {
-    state, // The state itself
-    setState, // The state update function
-  };
-
-  return (
-    // Set "val" as the value for "value" attribute:
-    <newContext.Provider value={value}>{props.children}</newContext.Provider>
-  );
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider>{children}</AppContext.Provider>;
 };
+
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export default { AppContext, AppProvider };
